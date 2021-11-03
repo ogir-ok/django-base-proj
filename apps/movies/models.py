9b90859c2b67a60b9dc956e3f109cd7ac8af9016
vladk1k <vlad02658@gmail.com>
2021-11-03 19:33:15 +0200

    django models

--- apps/movies/models.py
+++ apps/movies/models.py
@@ -1,16 +1,33 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.postgres.fields import ArrayField
 
 
-class Director(models.Model):
+TITLE_TYPE_CHOICE= []
+
+class Person(models.Model):
+    imdb_id = models.CharField('',max_length=255)
     name = models.CharField(_('Name'), max_length=255)
-    birth_date = models.DateField(_('Birth date'))
-
-    def __str__(self):
-        return self.name
+    birth_year = models.DateField(_('birthYear'), max_length=255)
+    death_year = models.DateField(_('deathYear'), max_length=255)
 
 
 class Movie(models.Model):
-    name = models.CharField(_('Name'), max_length=255)
-    release_date = models.DateField(_('Release date'))
-    director = models.ForeignKey(Director, on_delete=models.PROTECT)
+    imdb_id = models.CharField('',max_length=255)
+    title_type = models.CharField(_('titleType'), max_length=255, choices=TITLE_TYPE_CHOICE)
+    name = models.CharField(_('primaryName'), max_length=255)
+    is_adult = models.BooleanField(_('isAdult'), default=False)
+    year = models.DateField(_('startYear'))
+
+
+CATEGORY_CHOICE = []
+JOB_CHOICE = []
+
+
+class PersonMovie(models.Model):
+    movie_id = models.ForeignKey(Movie, on_delete=models.PROTECT)
+    person_id = models.ForeignKey(Person, on_delete=models.PROTECT)
+    order = models.CharField(_('ordering'), max_length=255)
+    category = models.CharField(_('category'),max_length=255, choices=CATEGORY_CHOICE)
+    job = models.CharField(_('job'),max_length=255, choices=JOB_CHOICE)
+    characters = ArrayField(ArrayField(models.CharField(_('characters'), max_length=255)))
