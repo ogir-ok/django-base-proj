eb2cdbabd805ebbb89390dc6aed0f9b718d36ae0
MrWhiteres <alexandr.leluk.99@gmail.com>
2021-11-05 16:00:59 +0200

    dz10

--- apps/movies/admin.py
+++ apps/movies/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-
-from apps.movies.models import Director, Movie
-
-
-@admin.register(Director)
-class DirectorAdmin(admin.ModelAdmin):
-    list_display = ['id',  'name']
+from apps.movies.models import Movie, Person, PersonMovie
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'director']
+    list_display = ['imdb_id', 'title_type', 'name', 'is_adult', 'year', 'genres']
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['imdb_id',  'name', 'birth_year', 'death_year']
+
+
+@admin.register(PersonMovie)
+class PersonMovieAdmin(admin.ModelAdmin):
+    list_display = ['movie_id', 'person_id', 'order', 'category', 'job', 'characters']
