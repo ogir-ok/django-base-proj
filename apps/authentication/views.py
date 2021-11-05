02e0584fdbc2d57a1306f5858069decadc8092bd
Ihor Harahatyi <ihor.harahatyi@atomcream.com>
2021-11-05 18:19:56 +0200

    Fixed postgres + auth

---
+++ apps/authentication/views.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def my_view(request):
+    return render(request, 'authentication/base.html')
--- urls.py
+++ urls.py
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include(('django.contrib.auth.urls', 'auth'), namespace='auth')),
+    path('authentication/', include('apps.authentication.urls', namespace='authentication'))
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
