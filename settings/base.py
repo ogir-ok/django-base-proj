06034dd9707391196c3d0eb721ded9b79dc857bd
Ihor Harahatyi <ihor.harahatyi@atomcream.com>
2021-11-06 02:07:07 +0200

    Lesson: django templates

--- settings/base.py
+++ settings/base.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from IPython.lib.clipboard import osx_clipboard_get
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- urls.py
+++ urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+import django.contrib.auth.urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include(('django.contrib.auth.urls', 'auth'), namespace='auth')),
