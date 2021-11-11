0c0ed3499fa899f17bd81ad2e9c76ed9b109ba6f
f4rk4sh <farkashyaroslav@gmail.com>
2021-11-11 20:10:07 +0200

    adding dz12

--- apps/movies/urls.py
+++ apps/movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import list_movies
+from .views import movies_list, movie_detail
 
 app_name = 'apps.movies'
 
 urlpatterns = [
-    path('', list_movies, name='list_movies'),
+    path('', movies_list, name='movies_list'),
+    path('movie/<int:pk>/', movie_detail, name='movie_detail'),
 ]
