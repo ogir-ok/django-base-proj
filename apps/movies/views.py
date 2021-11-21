6ebd98dc93317eaaa9c522d3959d4b71b667ee18
vladk1k <vlad02658@gmail.com>
2021-11-21 11:26:35 +0200

    add list movies and details page

--- apps/movies/views.py
+++ apps/movies/views.py
@@ -5,10 +5,15 @@ from django.shortcuts import render
 
 import logging
 
+from apps.movies.models import Movie
+
 logger = logging.getLogger(__name__)
 
 def list_movies(request):
-    return render(request, 'movies/list.html')
+    return render(request, 'movies/list.html', context={'movies': Movie.objects.order_by('-year').all()[:20]})
+
+def details_movies(request, **kwargs):
+    return render(request, 'movies/details.html', context={'movie': Movie.objects.get(id=kwargs.get('id'))})
 
 def long_running(request):
     logger.error('starting...')
