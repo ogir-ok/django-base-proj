82e860cdbc0b0cc348d7e3220068797691f731d7
MrWhiteres <alexandr.leluk.99@gmail.com>
2021-11-25 21:42:35 +0200

    upp

--- apps/movies/views.py
+++ apps/movies/views.py
@@ -3,8 +3,10 @@ from time import sleep
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.db import connection
 from apps.movies.forms import MovieForm, PersonForm
-from apps.movies.models import Movie, Person
+from apps.movies.models import *
+
 
 logger = logging.getLogger(__name__)
 
@@ -40,7 +42,22 @@ def person_add(request):
 
 
 def movie_and_person_rank(request):
-    return render(request, 'movies/movie and person rank.html')
+    cursor = connection.cursor()
+    cursor.execute("""SELECT movies_movie.name,sum(sq.occurences_count) as "rank"
+                        from movies_movie
+                        join movies_personmovie on movies_personmovie.movie_id = movies_movie.id
+                        left join (SELECT movies_person.id as "person_id", count(*) as "occurences_count"
+                                from movies_person
+                                LEFT OUTER join public.movies_personmovie on movies_person.id = movies_personmovie.person_id
+                                where 
+                                    movies_personmovie.category = 'actor' or
+                                    movies_personmovie.category = 'actress' or
+                                movies_personmovie.category = 'self'
+                                group by movies_person.id) as sq on sq.person_id = movies_personmovie.person_id
+                        group by movies_movie.id
+                        order by rank DESC;
+    """)
+    return render(request, 'movies/movie and person rank.html', context={'data': cursor})
 
 
 def long_running(request):
