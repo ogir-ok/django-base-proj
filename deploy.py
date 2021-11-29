0c337778bb7af9514736d730115af723c5e71a1f
Ihor Harahatyi <ihor.harahatyi@atomcream.com>
2021-11-29 19:58:48 +0200

    use local key

--- deploy.py
+++ deploy.py
@@ -11,7 +11,7 @@ class Pexpect:
     def __init__(self, host, default_expect='ubuntu@', timeout=300):
         self.host = host
         self.default_expect = default_expect
-        self.child = pexpect.spawn('ssh -i deploy-key.pem -o StrictHostKeyChecking=no ubuntu@{}'.format(host), timeout=timeout, encoding='utf-8')
+        self.child = pexpect.spawn('ssh -i deploy-key -o StrictHostKeyChecking=no ubuntu@{}'.format(host), timeout=timeout, encoding='utf-8')
         self.child.logfile = sys.stdout
         self.child.expect(default_expect)
 
@@ -60,7 +60,7 @@ def deploy_host(instance):
 def deploy():
     instances = find_instances()
 
-    with open('deploy-key.pem', 'w') as f:
+    with open('deploy-key', 'w') as f:
         f.write(os.getenv('SSH_KEY'))
 
     os.system('chmod 0500 deploy-key.pem')
