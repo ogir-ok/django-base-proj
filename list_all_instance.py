52ebe2c837a55821d453227b555d391b92132302
MrWhiteres <alexandr.leluk.99@gmail.com>
2021-12-04 23:48:57 +0200

    fix

--- list_all_instance.py
+++ list_all_instance.py
@@ -7,11 +7,17 @@ EC2_RESOURCE = boto3.resource('ec2', region_name='us-east-1')
 
 instances = EC2_RESOURCE.instances.all()
 
-for instance in instances:
-    print(f'EC2 instance {instance.id}" information:')
-    print(f'Instance state: {instance.state["Name"]}')
-    print(f'Instance AMI: {instance.image.id}')
-    print(f'Instance platform: {instance.platform}')
-    print(f'Instance type: "{instance.instance_type}')
-    print(f'Piblic IPv4 address: {instance.public_ip_address}')
-    print('-'*60)
+
+def list_instanc():
+    for instance in instances:
+        print(f'EC2 instance {instance.id}" information:')
+        print(f'Instance state: {instance.state["Name"]}')
+        print(f'Instance AMI: {instance.image.id}')
+        print(f'Instance platform: {instance.platform}')
+        print(f'Instance type: "{instance.instance_type}')
+        print(f'Piblic IPv4 address: {instance.public_ip_address}')
+        print('-' * 60)
+
+
+if __name__ == '__main__':
+    list_instanc()
